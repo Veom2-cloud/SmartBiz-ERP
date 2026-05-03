347de0c5d03ce8f8bcf8c0cd2aa276875971b1c3
veom <veomnohria@gmail.com>
2026-05-03 14:02:23 +0530

    added tests

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+# Add parent folder to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from run import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
